79af1579f0be110e4c818781f6f1d2a565a70fa9
JEFF HENRY <henry_jf@hotmail.com>
2016-04-12 15:14:55 -0400

    working on clock in console

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,11 @@
-console.log('Hello New Project!');
+ var clock = function() {
+  var hours = new Date().getHours();
+  var minutes = new Date().getMinutes();
+  var seconds = new Date().getSeconds();
+  console.log(hours, minutes, seconds);
+};
+var time = setInterval(clock, 1000);
+//  return clock;
+// console.log(clock);
+var timer = document.querySelector('#clock');
+inner.HTML
