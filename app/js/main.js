587b227f77f354d47e435fbf4d4ee2c0cf0864b5
JEFF HENRY <henry_jf@hotmail.com>
2016-04-12 15:54:23 -0400

    clock is working on screen

--- app/js/main.js
+++ app/js/main.js
@@ -2,10 +2,11 @@
   var hours = new Date().getHours();
   var minutes = new Date().getMinutes();
   var seconds = new Date().getSeconds();
-  console.log(hours, minutes, seconds);
+  // console.log(hours, minutes, seconds);
+  var clock = hours + ":" + minutes + ":" + seconds;
+
+  var timer = document.querySelector('#clock');
+  timer.innerHTML = clock;
 };
+
 var time = setInterval(clock, 1000);
-//  return clock;
-// console.log(clock);
-var timer = document.querySelector('#clock');
-inner.HTML
