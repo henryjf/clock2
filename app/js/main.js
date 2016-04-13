926e3d3407dd12e90f0fd9f8fdbb0ca66fccf7da
JEFF HENRY <henry_jf@hotmail.com>
2016-04-12 20:01:57 -0400

    finished js for first clock

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,16 @@
   var minutes = new Date().getMinutes();
   var seconds = new Date().getSeconds();
   // console.log(hours, minutes, seconds);
-  var clock = hours + ":" + minutes + ":" + seconds;
+  var hour = (hours > 12) ? hours - 12 : hours;
+  var newHour = (hour < 10) ? '0' + hour : hour;
+  var min = (minutes < 10) ? '0' + minutes : minutes;
+  var sec = (seconds< 10) ? '0' + seconds : seconds;
+
+  // if ( hour < 10)
+  //   return '0' + hour;
+
+
+  var clock = newHour + ":" + min + ":" + sec;
 
   var timer = document.querySelector('#clock');
   timer.innerHTML = clock;
