5f38b28fa0b038dedf82897e4a9e1dca88823637
JEFF HENRY <henry_jf@hotmail.com>
2016-04-12 20:30:23 -0400

    finished clock to display hexadecimal colors

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,25 @@
- var clock = function() {
+
+//Used the Date obj to get hours, minutes and seconds
+  var clock = function() {
   var hours = new Date().getHours();
   var minutes = new Date().getMinutes();
   var seconds = new Date().getSeconds();
-  // console.log(hours, minutes, seconds);
+
+//Used ternary statements to set digits to proper settings
   var hour = (hours > 12) ? hours - 12 : hours;
   var newHour = (hour < 10) ? '0' + hour : hour;
   var min = (minutes < 10) ? '0' + minutes : minutes;
   var sec = (seconds< 10) ? '0' + seconds : seconds;
 
-  // if ( hour < 10)
-  //   return '0' + hour;
+//Added expression to get ready for printing the correct code
+    var clock = '#' + newHour + min + sec;
 
-
-  var clock = newHour + ":" + min + ":" + sec;
-
+//Used document.querySelector to locate where js will be inputted to page
   var timer = document.querySelector('#clock');
+// Used innerHTML to display js to page
   timer.innerHTML = clock;
+// Used properties to access js to change background color in hexadecimal colors
+  timer.style.backgroundColor = clock;
 };
-
+//Used setInterval to call clock function every sec.
 var time = setInterval(clock, 1000);
